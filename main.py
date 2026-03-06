20c019baffd90c7bbbe8d5b10baca786f9fc3c7a
Anvesha Kumar <anv90@users.noreply.github.com>
2026-03-05 16:56:15 -0800

    added pseudocode

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+app.get
+    param:
+        num to convert
+        units to convert to
+    case (unit)
+    hrs -> divide by 60
+    days -> hrs, then divide by 24
+    returns the converted num
+
+
+
+
+"""
