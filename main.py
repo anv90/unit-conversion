dfd4795c64b7dcf228944a424708dfda82bc77a8
Anvesha Kumar <anv90@users.noreply.github.com>
2026-03-06 16:40:25 -0800

    rough skeleton with get method

--- main.py
+++ main.py
@@ -12,3 +12,26 @@ app.get
 
 
 """
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def calculate_conversion(num, unit):
+    case (unit)
+    #fill out different cases
+
+@app.get('/unit_conversion')
+def unit_conversion():
+    num = request.args.get('num')
+    unit = request.args.get('unit')
+    if num and unit:
+        #check for valid unit??
+        result =  calculate_conversion(num, unit)
+        return jsonify(result), 200
+    else:
+        return jsonify({"error": "Missing 'num' or 'unit' parameter"}), 400
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6001, debug=True)
